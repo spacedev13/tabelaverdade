510c77c7233046e905721aaa02ab826ca070025f
Davi Tavares <108487861+spacedev13@users.noreply.github.com>
2023-03-10 20:12:14 -0300

    Add files via upload

---
+++ tabela verdade.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int numero1,numero2,portaAnd;
+	printf("Digite o valor de X");
+	scanf("%i", &numero1);
+	printf("Digite o valor de Y");
+	scanf("%i", &numero2);
+	portaAnd=numero1 and numero2;
+	printf("o numero digitado foi: %i", portaAnd);
+}
+
